45769e30734d0c65be27cc60081b40353fdc9401
Ramsy de Vos <ramsy@ramsy.it>
2019-11-02 12:06:01 +0100

    Initial Commit

---
+++ Subscriptions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Subscriptions
+//
+//  Created by R.M. de Vos on 02/11/2019.
+//  Copyright © 2019 R.M. de Vos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
